66b15d2e674220d88589c6db3da4da2983240d3d
ManikantaNvsr <50462892+ManikantaNvsr@users.noreply.github.com>
2020-09-04 11:05:59 +0530

    All Testcases Passed

---
+++ src/main/java/dev/geeksforgeeks/mustdocodingquestions/linkedlists/MiddleElementInLinkedList.java
@@ -0,0 +1,27 @@
+package dev.geeksforgeeks.mustdocodingquestions.linkedlists;
+
+import dev.code.Node;
+
+//https://practice.geeksforgeeks.org/problems/finding-middle-element-in-a-linked-list/1
+public class MiddleElementInLinkedList {
+
+    public int getMiddle(Node head) {
+        Node speedPointer = head;
+        Node slowPointer = head;
+        // If it is only a single element.
+        if (head.next == null) {
+            return head.data;
+        }
+        // If it is of two elements.
+        if (head.next.next == null) {
+            return head.next.data;
+        }
+        // If it is greater than two elements, we hop the speed pointer two elements and the slow pointer one element
+        // By the time, the speed pointer reached the end of the list, the slow pointer would reach the middle element.
+        while (speedPointer != null && speedPointer.next != null) {
+            speedPointer = speedPointer.next.next;
+            slowPointer = slowPointer.next;
+        }
+        return slowPointer.data;
+    }
+}
