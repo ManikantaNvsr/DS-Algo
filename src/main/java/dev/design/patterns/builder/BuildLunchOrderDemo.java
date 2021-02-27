a13e0158d6bd33df6317e987ad6923e5c0bef6b9
ManikantaNvsr <50462892+ManikantaNvsr@users.noreply.github.com>
2021-02-27 13:05:43 +0530

    All Testcases Passed

---
+++ src/main/java/dev/design/patterns/builder/BuildLunchOrderDemo.java
@@ -0,0 +1,13 @@
+package dev.design.patterns.builder;
+
+public class BuildLunchOrderDemo {
+
+    public static void main(String[] args) {
+        LunchOrder lunchOrder = new LunchOrder.Builder().bread("Wheat").condiments("Lettuce").dressing("Mayo").meat("Turkey").build();
+
+        System.out.println(lunchOrder.getBread());
+        System.out.println(lunchOrder.getCondiments());
+        System.out.println(lunchOrder.getDressing());
+        System.out.println(lunchOrder.getMeat());
+    }
+}
