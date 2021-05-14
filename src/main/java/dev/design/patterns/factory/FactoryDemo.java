302895d2530bc50dc6300c35fe13e36a3570bac5
ManikantaNvsr <50462892+ManikantaNvsr@users.noreply.github.com>
2021-05-14 12:48:24 +0530

    All Testcases Passed

--- src/main/java/dev/design/patterns/factory/FactoryDemo.java
+++ src/main/java/dev/design/patterns/factory/FactoryDemo.java
@@ -2,9 +2,9 @@ package dev.design.patterns.factory;
 
 public class FactoryDemo {
     public static void main(String[] args) {
-//        Website site = WebsiteFactory.getWebsite(WebsiteType.BLOG);
-//        System.out.println(site.getPages());
-//        site = WebsiteFactory.getWebsite(WebsiteType.SHOP);
-//        System.out.println(site.getPages());
+        Website site = WebsiteFactory.getWebsite(WebsiteType.BLOG);
+        System.out.println(site.getPages());
+        site = WebsiteFactory.getWebsite(WebsiteType.SHOP);
+        System.out.println(site.getPages());
     }
 }
